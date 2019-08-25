8a74717191ef501fd500ad3f56d81ab1a1f73871
Matheus Abreu <matheusa596@gmail.com>
2019-08-25 08:28:43 -0300

    Criação da modelo de Membro.

---
+++ src/app/shared/membro.spec.ts
@@ -0,0 +1,7 @@
+import { Membro } from './membro';
+
+describe('Membros', () => {
+  it('should create an instance', () => {
+    expect(new Membro()).toBeTruthy();
+  });
+});
---
+++ src/app/shared/membro.ts
@@ -0,0 +1,3 @@
+export class Membro {
+    nome: string;
+}
